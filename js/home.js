36c01ce28455417fa57ae9069f70d69dd4798abb
record001 <umarkhidirboev@gmail.com>
2022-08-11 13:59:55 +0500

    first commit

---
+++ js/home.js
@@ -0,0 +1,87 @@
+function renderProductsToSlider() {
+    let productFrag = document.createDocumentFragment()
+
+    PRODUCT_LIST.forEach(item => {
+
+        let swiperSlider = document.createElement("div")
+        
+        swiperSlider.setAttribute("class","swiper-slide")
+        swiperSlider.innerHTML = `
+        <img class="product__img" src=${item.image} alt="coctail_funkin1">
+    
+        <h4 class="product__title">${item.title}</h4>
+        <p class="product__description">${item.describtion}</p>
+        <a class="product__cta" href="#!">Shop now</a>
+        `
+        productFrag.appendChild(swiperSlider)
+    })
+  
+
+    document.querySelector(".swiper-wrapper").appendChild(productFrag)
+}
+renderProductsToSlider()
+
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      600: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 50,
+      },
+    },
+    
+  });
+
+  var swiper1 = new Swiper(".mySwiper1", {
+    slidesPerView: 3,
+    // spaceBetween: 60,
+    slidesPerGroup: 1,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 20,
+      },
+      600: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 40,
+      },
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 50,
+      },
+    },
+    
+  });
---
+++ js/products.js
@@ -0,0 +1,50 @@
+const PRODUCT_LIST = [
+    {
+        id:1,
+        image:"./images/home/coctail_funkin1.png",
+        title:"Ipsum dolor",
+        describtion: "Ut enim ad minim veniam, quis nostrud exerp ex ea commodo consequat.",
+    },
+    {
+        id:2,
+        image:"./images/home/coctail_funkin2.png",
+        title:"Consectetur adipiscing",
+        describtion: "Ut enim ad minim veniam, quis nostrud exerp ex ea commodo consequat.",
+    },
+    {
+        id:3,
+        image:"./images/home/coctail_funkin3.png",
+        title:"Consequat",
+        describtion: "Ut enim ad minim veniam, quis nostrud exerp ex ea commodo consequat.",
+    },
+    {
+        id:4,
+        image:"./images/home/coctail_funkin4.png",
+        title:"Commodo",
+        describtion: "Ut enim ad minim veniam, quis nostrud exerp ex ea commodo consequat.",
+    },
+    {
+        id:1,
+        image:"./images/home/coctail_funkin1.png",
+        title:"Ipsum dolor",
+        describtion: "Ut enim ad minim veniam, quis nostrud exerp ex ea commodo consequat.",
+    },
+    {
+        id:2,
+        image:"./images/home/coctail_funkin2.png",
+        title:"Consectetur adipiscing",
+        describtion: "Ut enim ad minim veniam, quis nostrud exerp ex ea commodo consequat.",
+    },
+    {
+        id:3,
+        image:"./images/home/coctail_funkin3.png",
+        title:"Consequat",
+        describtion: "Ut enim ad minim veniam, quis nostrud exerp ex ea commodo consequat.",
+    },
+    {
+        id:4,
+        image:"./images/home/coctail_funkin4.png",
+        title:"Commodo",
+        describtion: "Ut enim ad minim veniam, quis nostrud exerp ex ea commodo consequat.",
+    },
+]
